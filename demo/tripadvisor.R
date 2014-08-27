992712c4b5a255917e6e228cc65cfc7e7490ca39
hadley <h.wickham@gmail.com>
2014-08-27 13:42:19 -0500

    Add tripadvisor scraping demo

---
+++ demo/tripadvisor.R
@@ -0,0 +1,37 @@
+# Inspired by
+# http://notesofdabbler.github.io/201408_hotelReview/scrapeTripAdvisor.html
+
+library(rvest)
+library(magrittr)
+
+url <- "http://www.tripadvisor.com/Hotel_Review-g37209-d1762915-Reviews-JW_Marriott_Indianapolis-Indianapolis_Indiana.html"
+
+reviews <- url %>%
+  html() %>%
+  html_node("#REVIEWS .innerBubble")
+
+id <- reviews %>%
+  html_node(".quote a") %>%
+  html_attr("id")
+
+quote <- reviews %>%
+  html_node(".quote span") %>%
+  html_text()
+
+rating <- reviews %>%
+  html_node(".rating .rating_s_fill") %>%
+  html_attr("alt") %>%
+  gsub(" of 5 stars", "", .) %>%
+  as.integer()
+
+date <- reviews %>%
+  html_node(".rating .ratingDate") %>%
+  html_attr("title") %>%
+  strptime("%b %d, %Y") %>%
+  as.POSIXct()
+
+review <- reviews %>%
+  html_node(".partial_entry") %>%
+  html_text()
+
+data.frame(id, quote, rating, date, review, stringsAsFactors = FALSE) %>% View()
