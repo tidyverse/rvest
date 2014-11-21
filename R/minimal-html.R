189f32dab0a9e79ac77c52d9c575170c31e9d92d
hadley <h.wickham@gmail.com>
2014-11-21 09:53:59 -0600

    Helper to generate minimal html page

---
+++ R/minimal-html.R
@@ -0,0 +1,19 @@
+#' Generate a minimal html5 page.
+#'
+#' See \url{http://www.brucelawson.co.uk/2010/a-minimal-html5-document/} for
+#' details.
+#'
+#' @param title Page title
+#' @param html Other html to insert into page.
+#' @keywords internal
+#' @export
+#' @examples
+#' minimal_html("test")
+minimal_html <- function(title, html = "") {
+  html(paste0(
+    "<!doctype html>\n",
+    "<meta charset=utf-8>\n",
+    "<title>", title, "</title>\n",
+    html
+  ))
+}
