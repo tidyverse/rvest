2b09ca6342cd21815664885042d0c290193d627d
hadley <h.wickham@gmail.com>
2016-06-15 17:04:13 -0500

    Update zillow demo
    Fixes #138. Closes #139.

--- demo/zillow.R
+++ demo/zillow.R
@@ -2,49 +2,27 @@
 library(rvest)
 library(tidyr)
 
-url <- "http://www.zillow.com/homes/for_sale/Greenwood-IN/fsba,fsbo,fore,cmsn_lt/house_type/52333_rid/39.638414,-86.011362,39.550714,-86.179419_rect/12_zm/0_mmm/"
+page <- read_html("http://www.zillow.com/homes/for_sale/Greenwood-IN/fsba,fsbo,fore,cmsn_lt/house_type/52333_rid/39.638414,-86.011362,39.550714,-86.179419_rect/12_zm/0_mmm/")
 
-houses <- read_html(url) %>%
-  html_nodes("article")
+houses <- page %>%
+  html_nodes(".photo-cards li article")
 
 z_id <- houses %>% html_attr("id")
 
 address <- houses %>%
-  html_node(".property-address a") %>%
-  html_attr("href") %>%
-  strsplit("/") %>%
-  pluck(3, character(1))
-
-area <- function(x) {
-  val <- as.numeric(gsub("[^0-9.]+", "", x))
-  as.integer(val * ifelse(grepl("ac", x), 43560, 1))
-}
-sqft <- houses %>%
-  html_node(".property-lot") %>%
-  html_text() %>%
-  area()
-
-year_build <- houses %>%
-  html_node(".property-year") %>%
-  html_text() %>%
-  gsub("Built in ", "", .) %>%
-  as.integer()
+  html_node(".zsg-photo-card-address") %>%
+  html_text()
 
 price <- houses %>%
-  html_node(".price-large") %>%
+  html_node(".zsg-photo-card-price") %>%
   html_text() %>%
-  tidyr::extract_numeric()
+  readr::parse_number()
 
 params <- houses %>%
-  html_node(".property-data") %>%
+  html_node(".zsg-photo-card-info") %>%
   html_text() %>%
-  strsplit(", ")
-beds <- params %>%
-  pluck(1, character(1)) %>%
-  extract_numeric()
-baths <- params %>%
-  pluck(2, character(1)) %>%
-  extract_numeric()
-house_area <- params %>%
-  pluck(3, character(1)) %>%
-  area()
+  strsplit("·")
+
+beds <- params %>% purrr::map_chr(1) %>% readr::parse_number()
+baths <- params %>% purrr::map_chr(2) %>% readr::parse_number()
+house_area <- params %>% purrr::map_chr(3) %>% readr::parse_number()
