951f667b2d1fb72281f2dd053c0f07f53a0baba3
hadley <h.wickham@gmail.com>
2016-06-15 16:44:35 -0500

    Replace encoding example with static file
    Fixes #154

--- R/encoding.R
+++ R/encoding.R
@@ -15,24 +15,15 @@
 #' @param from The encoding that the string is actually in. If \code{NULL},
 #' @name encoding
 #' @examples
-#' \donttest{
-#' # This page claims to be in iso-8859-1:
-#' url <- 'http://www.elections.ca/content.aspx?section=res&dir=cir/list&document=index&lang=e#list'
-#' elections <- read_html(url)
-#' x <- elections %>% html_nodes("table") %>% .[[2]] %>% html_table() %>% .$TO
-#' # But something looks wrong:
-#' x
+#' # A file with bad encoding included in the package
+#' path <- system.file("html-ex", "bad-encoding.html", package = "rvest")
+#' x <- read_html(path)
+#' x %>% html_nodes("p") %>% html_text()
 #'
-#' # It's acutally UTF-8!
 #' guess_encoding(x)
-#'
-#' # We can repair this vector:
-#' repair_encoding(x)
-#'
-#' # But it's better to start from scratch with correctly encoded file
-#' elections <- read_html(url, encoding = "UTF-8")
-#' elections %>% html_nodes("table") %>% .[[2]] %>% html_table() %>% .$TO
-#' }
+#' # Two valid encodings, only one of which is correct
+#' read_html(path, encoding = "ISO-8859-1") %>% html_nodes("p") %>% html_text()
+#' read_html(path, encoding = "ISO-8859-2") %>% html_nodes("p") %>% html_text()
 NULL
 
 #' @rdname encoding
