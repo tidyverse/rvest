385561f9e549474efed9cb23a7555f02c5c1eb50
hadley <h.wickham@gmail.com>
2016-06-15 17:16:12 -0500

    Update united demo #138

--- demo/united.R
+++ demo/united.R
@@ -1,21 +1,20 @@
 # Scrape miles from united site
-
 library(rvest)
-library(magrittr)
 
 united <- html_session("http://www.united.com/")
-account <- united %>% follow_link("Account")
 
-login <- account %>%
-  html_nodes("form") %>%
-  extract2(1) %>%
+login <- united %>%
+  html_node("form[name=LoginForm]") %>%
   html_form() %>%
-   set_values(
-    `ctl00$ContentInfo$SignIn$onepass$txtField` = "GY797363",
-    `ctl00$ContentInfo$SignIn$password$txtPassword` = password
+  set_values(
+    MpNumber = "GY797363",
+    Password = password
   )
-account <- account %>%
-  submit_form(login, "ctl00$ContentInfo$SignInSecure")
 
-logged_in %>% jump_to(gsub(" ", "+", headers(logged_in)$Location))
+logged_in <- united %>% submit_form(login)
 
+logged_in %>%
+  follow_link("View account") %>%
+  html_node("#ctl00_ContentInfo_AccountSummary_spanEliteMilesNew") %>%
+  html_text() %>%
+  readr::parse_number()
