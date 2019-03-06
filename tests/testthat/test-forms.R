d0c9e3b24cee17c02b2f50a244e3bdddd8dca570
richard <richard@sapaso.de>
2019-03-06 21:48:50 +0100

    Correct changes to hadley's recommondations
    https://github.com/tidyverse/rvest/pull/161

--- tests/testthat/test-forms.R
+++ tests/testthat/test-forms.R
@@ -2,13 +2,13 @@ context("form parsing")
 
 test_that("select options are named character vector", {
   select <- minimal_html("select parsing", '
-    <form>
-      <select name="x">
-        <option value="1">a</option>
-        <option value="2">b</option>
-      </select>
-    </form>
-  ')
+                         <form>
+                         <select name="x">
+                         <option value="1">a</option>
+                         <option value="2">b</option>
+                         </select>
+                         </form>
+                         ')
 
   form <- select %>% html_node("form") %>% html_form()
   expect_equal(form$fields[[1]]$options, c(a = "1", b = "2"))
@@ -16,21 +16,21 @@ test_that("select options are named character vector", {
 
 test_that("select values are inherited from names", {
   page <- minimal_html("optional values", '
-    <select name="b" id="a">
-      <option value="1">x</option>
-      <option>y</option>
-    </select>
-  ')
+                       <select name="b" id="a">
+                       <option value="1">x</option>
+                       <option>y</option>
+                       </select>
+                       ')
   opts <- page %>% html_node('select') %>% parse_select()
   expect_equal(opts$options, c(x = "1", y = "y"))
 })
 
 test_that("parse_fields gets the button", {
-    select <- minimal_html("button test", '
-    <form>
-      <button type="submit">Click me</button>
-    </form>
-  ')
+  select <- minimal_html("button test", '
+                         <form>
+                         <button type="submit">Click me</button>
+                         </form>
+                         ')
 
   form <- select %>% html_node("form") %>% html_form()
   expect_equal(form$fields[[1]]$type, "submit")
@@ -40,18 +40,15 @@ test_that("submit_request detects submit-buttons correct", {
   select <- minimal_html("submit test", '
                          <form id="suchparameterForm" method="post" action="/test-path">
                          <select name="elementWithoutType" size="1">
-                          <option value="10">10</option>
-								          <option value="25">25</option>
+                         <option value="10">10</option>
+                         <option value="25">25</option>
                          </select>
                          <button type="submit" name="clickMe">Click me</button>
                          </form>
                          ')
-
   form <- select %>% html_node("form") %>% html_form()
-  expect_null(form$fields[[1]]$type)
-  expect_equal(form$fields[[2]]$type, "submit")
 
-  req <- rvest:::submit_request(form, "clickMe")
+  req <- submit_request(form, "clickMe")
   expect_length(req, 4L)
   expect_equal(req$method, "POST")
   expect_equal(req$url, "/test-path")
