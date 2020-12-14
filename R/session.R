de73a8100de61a1d558b4200ed0669df98b75bfd
Hadley Wickham <h.wickham@gmail.com>
2020-12-14 15:00:44 -0600

    Move read_xml.session into session.R

--- R/session.R
+++ R/session.R
@@ -63,6 +63,21 @@ request_POST <- function(x, url, ...) {
   x
 }
 
+#' @importFrom xml2 read_xml
+#' @export
+read_xml.session <- function(x, ..., as_html = FALSE) {
+  if (exists("cached", envir = x$html)) {
+    return(x$html$cached)
+  }
+
+  if (!is_html(x)) {
+    stop("Current page doesn't appear to be html.", call. = FALSE)
+  }
+
+  read_xml(x$response, ..., as_html = as_html)
+}
+
+
 show <- function(x) {
   temp <- tempfile()
   writeBin(httr::content(x$response, "raw"), temp)
