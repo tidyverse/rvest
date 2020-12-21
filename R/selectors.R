45dff00c3211e9643af2103158191b39d2140262
Hadley Wickham <h.wickham@gmail.com>
2020-12-21 07:51:35 -0600

    Improve html_node docs

--- R/selectors.R
+++ R/selectors.R
@@ -1,17 +1,12 @@
 #' Select nodes from an HTML document
 #'
-#' More easily extract pieces out of HTML documents using XPath and CSS
-#' selectors. CSS selectors are particularly useful in conjunction with
-#' <http://selectorgadget.com/>: it makes it easy to find exactly
-#' which selector you should be using. If you haven't used CSS selectors
-#' before, work your way through the fun tutorial at
-#' <http://flukeout.github.io/>
+#' `html_node()` and `html_nodes()` find HTML tags (nodes) using CSS selectors
+#' XPath expressions.
 #'
-#' @section `html_node` vs `html_nodes`:
-#' `html_node` is like `[[` it always extracts exactly one
-#' element. When given a list of nodes, `html_node` will always return
-#' a list of the same length, the length of `html_nodes` might be longer
-#' or shorter.
+#' CSS selectors are particularly useful in conjunction with
+#' <https://selectorgadget.com/>, which makes it very easy to discover the
+#' selector you need. If you haven't used CSS selectors before, I'd recommend
+#' starting with the the fun tutorial at <http://flukeout.github.io/>.
 #'
 #' @section CSS selector support:
 #'
@@ -23,25 +18,24 @@
 #' <http://www.w3.org/TR/2011/REC-css3-selectors-20110929/>. The
 #' exceptions are listed below:
 #'
-#' \itemize{
-#' \item Pseudo selectors that require interactivity are ignored:
-#'   `:hover`, `:active`, `:focus`, `:target`,
-#'   `:visited`
-#' \item The following pseudo classes don't work with the wild card element, *:
+#' * Pseudo selectors that require interactivity are ignored:
+#'   `:hover`, `:active`, `:focus`, `:target`, `:visited`.
+#' * The following pseudo classes don't work with the wild card element, *:
 #'   `*:first-of-type`, `*:last-of-type`, `*:nth-of-type`,
 #'   `*:nth-last-of-type`, `*:only-of-type`
-#' \item It supports `:contains(text)`
-#' \item You can use !=, `[foo!=bar]` is the same as `:not([foo=bar])`
-#' \item `:not()` accepts a sequence of simple selectors, not just single
+#' * It supports `:contains(text)`
+#' * You can use !=, `[foo!=bar]` is the same as `:not([foo=bar])`
+#' * `:not()` accepts a sequence of simple selectors, not just a single
 #'   simple selector.
-#' }
 #'
 #' @param x Either a document, a node set or a single node.
 #' @param css,xpath Nodes to select. Supply one of `css` or `xpath`
 #'   depending on whether you want to use a CSS or XPath 1.0 selector.
+#' @returns `html_node()` returns a nodeset the same length as the input.
+#'   `html_nodes()` flattens the output so there's no direct way to map
+#'   the output to the input.
 #' @export
 #' @examples
-#' # CSS selectors ----------------------------------------------
 #' url <- paste0(
 #'   "https://web.archive.org/web/20190202054736/",
 #'   "https://www.boxofficemojo.com/movies/?id=ateam.htm"
@@ -51,37 +45,23 @@
 #' html_nodes(ateam, "center font")
 #' html_nodes(ateam, "center font b")
 #'
-#' # But html_node is best used in conjunction with %>% from magrittr
-#' # You can chain subsetting:
+#' # html_nodes() well suited to use with the pipe
 #' ateam %>% html_nodes("center") %>% html_nodes("td")
 #' ateam %>% html_nodes("center") %>% html_nodes("font")
 #'
 #' td <- ateam %>% html_nodes("center") %>% html_nodes("td")
 #' td
-#' # When applied to a list of nodes, html_nodes() returns all nodes,
-#' # collapsing results into a new nodelist.
+#' # When applied to a list of nodes, html_nodes() returns all matching nodes
+#' # beneath any of the elements, flattening results into a new nodelist.
 #' td %>% html_nodes("font")
+#'
 #' # html_node() returns the first matching node. If there are no matching
 #' # nodes, it returns a "missing" node
 #' td %>% html_node("font")
 #'
-#' # To pick out an element at specified position, use magrittr::extract2
-#' # which is an alias for [[
-#' library(magrittr)
-#' ateam %>% html_nodes("table") %>% extract2(1) %>% html_nodes("img")
-#' ateam %>% html_nodes("table") %>% `[[`(1) %>% html_nodes("img")
-#'
-#' # Find all images contained in the first two tables
-#' ateam %>% html_nodes("table") %>% `[`(1:2) %>% html_nodes("img")
-#' ateam %>% html_nodes("table") %>% extract(1:2) %>% html_nodes("img")
-#'
-#' # XPath selectors ---------------------------------------------
-#' # chaining with XPath is a little trickier - you may need to vary
-#' # the prefix you're using - // always selects from the root node
-#' # regardless of where you currently are in the doc
-#' ateam %>%
-#'   html_nodes(xpath = "//center//font//b") %>%
-#'   html_nodes(xpath = "//b")
+#' # To pick out an element or elements at specified positions, use [[ and [
+#' ateam %>% html_nodes("table") %>% .[[1]] %>% html_nodes("img")
+#' ateam %>% html_nodes("table") %>% .[1:2] %>% html_nodes("img")
 html_nodes <- function(x, css, xpath) {
   UseMethod("html_nodes")
 }
