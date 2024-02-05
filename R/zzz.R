84a10d9d0401558a1e3559642d97786e4ae9bbdb
Hadley Wickham <h.wickham@gmail.com>
2024-02-05 11:20:50 -0600

    Make logic safer

--- R/zzz.R
+++ R/zzz.R
@@ -4,8 +4,8 @@ new_chromote <- NULL
   if (is_installed("chromote")) {
     new_chromote <<- utils::packageVersion("chromote") >= "0.1.2.9000"
   } else {
-    # If chromote is not installed yet, we assume the new version
-    new_chromote <- TRUE
+    # If chromote is not installed yet, assume it's not new to be safe.
+    new_chromote <- FALSE
   }
 
 
