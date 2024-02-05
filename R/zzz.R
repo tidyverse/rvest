0fc9b1af1119d58f0656dee6f6cc5fa0691a101d
Hadley Wickham <h.wickham@gmail.com>
2024-02-05 11:20:26 -0600

    Condition on installation status

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,13 @@
 new_chromote <- NULL
 
 .onLoad <- function(...) {
-  new_chromote <<- utils::packageVersion("chromote") >= "0.1.2.9000"
+  if (is_installed("chromote")) {
+    new_chromote <<- utils::packageVersion("chromote") >= "0.1.2.9000"
+  } else {
+    # If chromote is not installed yet, we assume the new version
+    new_chromote <- TRUE
+  }
+
 
   invisible()
 }
