a3eeaed39a5b37e2a07d0de1400305c0429762a2
Johannes Gruber <JohannesB.Gruber@gmail.com>
2026-09-24 19:51:00 +0100

    Fix bug when chromote is installed after rvest is loaded (#461)
    Fixes #417

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,7 @@ new_chromote <- NULL
     new_chromote <<- utils::packageVersion("chromote") >= "0.1.2.9000"
   } else {
     # If chromote is not installed yet, assume it's not new to be safe.
-    new_chromote <- FALSE
+    new_chromote <<- FALSE
   }
 
   invisible()
